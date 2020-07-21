2a84f25096eaffe65c4d7e0edc828a941a8789b5
suchenguo <65109015+guozhenduo@users.noreply.github.com>
2020-07-21 13:05:38 +0800

    Create while_true.c

---
+++ while_true.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+Author : guozhenduo1@163.com
+If you have gcc, installed, you can run the following code.
+ Keep printing "hello".
+WARING:This file name must be while_true.c,otherwise, it won't run.
+*/
+
+int main(void)
+{
+    printf("hello \n");
+    system("gcc while_true.c");
+    system("./a.out")
+}
